b05e04700f817fdc4cee06e7278afc96905389f0
itztla <hanumanman37@gmail.com>
2024-04-09 22:40:57 +0700

    refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,14 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'rebelot/kanagawa.nvim',
+  -- 'rebelot/kanagawa.nvim',
+  -- "ellisonleao/gruvbox.nvim",
+  "catppuccin/nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'kanagawa'
+    vim.o.background = "dark"
+    vim.cmd.colorscheme 'catppuccin-mocha'
     -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    -- vim.cmd.hi 'Comment gui=none'
   end,
 }
