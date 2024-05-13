25eba6f2738e0e672e8856453ca266d4af2f8fdd
itztla <hanumanman37@gmail.com>
2024-05-14 01:28:48 +0700

    fix git blame message

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -3,7 +3,9 @@ return {
   config = function()
     require('gitblame').setup {
       enabled = false,
+      date_format = '%r',
     }
+    require('lua-timeago').set_language(require 'lua-timeago/languages/en')
   end,
   keys = { { '<leader>gb', '<cmd>GitBlameToggle<cr>', desc = 'Toggle Git Blame' } },
 }
