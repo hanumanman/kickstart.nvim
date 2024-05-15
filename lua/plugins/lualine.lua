d7de0e73370101d71e3cde281ab1b38310ef1996
itztla <hanumanman37@gmail.com>
2024-05-15 23:51:36 +0700

    add macro status to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'nvim-lualine/lualine.nvim' },
   config = function()
     require('lualine').setup {
       options = {
@@ -8,6 +8,16 @@ return {
       },
       sections = {
         lualine_c = { { 'filename', path = 1 } },
+        lualine_x = {
+          {
+            require('noice').api.statusline.mode.get,
+            cond = require('noice').api.statusline.mode.has,
+            color = { fg = '#000' },
+          },
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
       },
     }
   end,
