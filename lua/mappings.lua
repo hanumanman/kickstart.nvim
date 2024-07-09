38483d73c82afd6c0cf50156a70e288084a2e920
hoang.nguyen14 <hoang.nguyen14@sotatek.com>
2024-07-09 22:03:20 +0700

    add turbo log

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -23,3 +23,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Yank word and Insert console.log() of that word in next line
+vim.keymap.set('n', '<leader>l', 'yawoconsole.log("Logging <C-r>0", <C-r>0);<esc>')
